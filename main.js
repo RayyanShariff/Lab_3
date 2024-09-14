da36c01c7c2d157267e9ee61068f0dcb2e5ff8ff
Rayyan Shariff <srs2000@hw.ac.uk>
2024-09-14 20:29:19 +0400

    Refactor index.html and main.js files

--- main.js
+++ main.js
@@ -1,85 +1,30 @@
-//Exercise 1
-$(document).ready(function(){ 
+$(document).ready(function() {
+  // Exercise 1
+  $("p").on("click", function() {
+    $(this).hide();
+  });
 
-  $("p").on("click",function(){ 
-  
-    $(this).hide() 
-  
-  }); 
-  
-}) 
-  
-   
-//Exercise 2
-//1  
-$(document).ready(function(){ 
+  // Exercise 2
+  // 1
+  $("body").prepend('<div>This is a Welcome message written using JQuery</div>');
 
-  $("body").append('<div>This is a Welcome message witten using JQuery</div>') 
+  // 2
+  $("body").append('<div class="colour">div1 yellow</div>');
+  $("body").append('<div class="colour">div2 yellow</div>');
+  $(".colour").css('background-color', 'yellow');
 
-}) 
+  // 3
+  $("body").append('<div class="hide">Hidden Message!</div>');
+  $("body").append('<button>Hide</button>');
+  $("button").click(function() {
+    $(".hide").toggle();
+  });
 
-//2
-$(document).ready(function(){ 
-
-  $("body").append('<div class = "colour">div1 yellow</div>') 
-
-}) 
-
-$(document).ready(function(){ 
-
-  $("body").append('<div class = "colour">div2 yellow</div>') 
-
-}) 
-
-$(document).ready(function(){ 
-
-  $(".colour").css('background-color','yellow') 
-
-}) 
-
-//3
-$(document).ready(function(){
-
-  $("body").append('<div class = "hide">Hidden Message!</div>')
-
-})
-
-$(document).ready(function(){
-
-  $("body").append('<button>Hide</button>')
-
-})
-
-$(document).ready(function(){ 
-
-  $("button").click(function(){ 
-
-    $(".hide").toggle() 
-
-  }) 
-
-}) 
-
-//5
-$(document).ready(function(){ 
-
-  $("body").append('<div style = "color:white" id = "hiddenName">Rayyan</div>') 
-
-}) 
-
-
-$(document).ready(function(){
-
-  $("body").append('<button id = "showButton">Show Name</button>')
-
-})
-
-$(document).ready(function(){ 
-
-  $("#showButton").click(function(){ 
-
+  // 5
+  $("body").append('<div style="color:white" id="hiddenName">Rayyan</div>');
+  $("body").append('<button id="showButton">Show Name</button>');
+  $("#showButton").click(function() {
     $("#hiddenName").css("color", "black");
+  });
 
-  }) 
-
-}) 
+});
